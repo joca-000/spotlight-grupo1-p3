162840b070bf36e940fbe7c4e46b92b3927d4315
joca-000 <163087200+joca-000@users.noreply.github.com>
2025-05-10 21:42:54 -0300

    feature de cadastro e login adicionadas.
    note que o cadastro apenas cadastra admins

--- Usuario.java
+++ Usuario.java
@@ -1,17 +1,54 @@
-public class Usuario {
-    private String login;
-    private String senha;
-
-    public Usuario(String login, String senha) {
-        this.login = login;
-        this.senha = senha;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
+public class Usuario{
+	private String id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String tipo; // admin, gerente, funcionario
+	
+	//construtor
+	public Usuario(String id, String nome, String email, String senha, String tipo) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.tipo = tipo;
+	}
+	
+	//getters e setters
+	public String getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public String setId() {
+		this.id = id;
+	}
+	public String setNome() {
+		this.nome = nome;
+	}
+	public String setEmail() {
+		this.email = email;
+	}
+	public String setSenha() {
+		this.senha = senha;
+	}
+	public String setTipo() {
+		this.tipo = tipo;
+	}
+	
+	//metodo para verificar senha
+	public boolean verificarSenha(String senha) {
+		return this.senha.equals(senha);
+	}
 }
